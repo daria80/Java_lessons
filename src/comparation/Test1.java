df1aa8f19ea2375a0e2536e6adc870bfebbfb09c
smirnova <dddsssmmm555@mail.ru>
2021-07-02 21:00:35 +0300

    Initial commit

---
+++ src/comparation/Test1.java
@@ -0,0 +1,19 @@
+package comparation;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Test1 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Smirnova");
+        list.add("Darya");
+        list.add("Mikhailovna");
+        System.out.println("Before sortage");
+        System.out.println(list);
+        System.out.println("After sortage");
+        Collections.sort(list);
+        System.out.println(list);
+    }
+}
