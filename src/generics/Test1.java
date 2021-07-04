3ffa26732bd9b8b112fe226d913b61d6bbe3299d
smirnova <dddsssmmm555@mail.ru>
2021-07-04 11:53:12 +0300

    add changes

---
+++ src/generics/Test1.java
@@ -0,0 +1,26 @@
+package generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        List list = new ArrayList<>();
+//        list.add("Ok");
+//        list.add(5);
+//        list.add(new StringBuilder("efnakhiu"));
+//        list.add(new Car());
+
+        list.add("HI");
+        list.add("BYE");
+        list.add("MY");
+        list.add("YOUR");
+
+        for(Object o : list) {
+            System.out.println(o + "length" + ((String)o).length());
+        }
+    }
+}
+class Car{}
